cc562ef82d084496431b1f51c486769f0db0ea1c
ttahm3d <ttahm3d@gmail.com>
2021-11-30 12:27:27 +0530

    feat: added reducer tests

--- src/redux/reducers/todos.test.js
+++ src/redux/reducers/todos.test.js
@@ -1,1 +1,24 @@
-test("should load the todos", () => {});
+import todosReducer from "./todos";
+
+describe("Todo Reducer", () => {
+	it("should load the initial state", () => {
+		expect(todosReducer({}, {})).toEqual({});
+	});
+
+	it("should load the todos on FETCH_TODOS Request", () => {
+		const todosMockData = [
+			{
+				id: "1",
+				text: "Some random text",
+			},
+		];
+
+		const successAction = {
+			type: "SET_TODOS",
+			payload: todosMockData,
+		};
+		expect(todosReducer({ todos: [] }, successAction)).toEqual({
+			todos: todosMockData,
+		});
+	});
+});
